a7ac5820f433ea5a94b280cc868fb632b4d35a4b
cator29 <2900081655@qq.com>
2024-08-12 16:45:52 +0800

    去除了对"../dist/output.css"的引用

--- src/Pages/CreateCircle.jsx
+++ src/Pages/CreateCircle.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import {useNavigate} from "react-router-dom";
 import 'tailwindcss/tailwind.css';
-import axios from 'axios';
 
 import {getCurrentUser} from "../util/currentUser.util.js";
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import '../dist/output.css'
-import {Route} from "react-router-dom";
-import {BrowserRouter} from "react-router-dom";
+//import '../dist/output.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
